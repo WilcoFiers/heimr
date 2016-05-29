3104b8e1854dc7822d89abecaae97728720ef12e
Wilco Fiers <wilco@>
2016-05-29 16:05:59 +0200

    scripted dynamic tabs

--- scripts/views.js
+++ scripts/views.js
@@ -29,26 +29,62 @@ function convertName(name) {
 		$('.domain-select').bind('click', function (e) {
 			var domainName = $(this).data('name');
 			views.domain(domainName, 'skill');
+			e.preventDefault();
 		});
 	};
-
 
 	// Show
 	views.domain = function (name, type) {
 		var domain = heimr.domains[name];
-		var list = domain[type];
-		console.log(list);
+		var list = domain[type] || [];
+
 		var data = {
 			name: convertName(name),
-			list: list
+			list: list,
+			current: type,
+			types: Object.keys(domain)
 		};
 
 		var html = templates.domain(data);
 		$('#main').html(html);
+
+		// Change tabs
+		$('a.detail-type').on('click', function (e) {
+			var type = $(this).data('type');
+			views.domain(name, type);
+			e.preventDefault();
+		});
+
+		// Show detail
+		$('a.select-detail').on('click', function (e) {
+			var index = $(this).data('index');
+			var detail = domain[type][index];
+			views.detail(name, type, detail);
+			e.preventDefault();
+		});
 	};
 
-	views.detail = function (domain, type, name) {
+	views.detail = function (domain, type, detail) {
+		var data = Object.assign({
+			domain: convertName(domain),
+			type: type,
+		}, detail);
 
+		var html = templates.detail(data);
+		$('#main').html(html);
+
+		// Change tabs
+		$('a.back').on('click', function (e) {
+			views.domain(domain, type);
+			e.preventDefault();
+		});
+
+		// Show detail
+		$('a.select-detail').on('click', function (e) {
+			var index = $(this).data('index');
+			var detail = domain[type][index];
+			views.detail(name, type, detail);
+		});
 	};
 
 })(jQuery, window.heimr);
